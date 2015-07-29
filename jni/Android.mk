4b113fe8de755247dc4a3a8530f7598ec4368ff4
Sasha <murzina93@gmail.com>
2015-07-29 16:18:37 +0600

    [*] Fixed Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,12 +1,13 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := callbacks
 LOCAL_SRC_FILES := callbacks.c
+include $(BUILD_SHARED_LIBRARY)
 
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := test
 LOCAL_SRC_FILES := test.c
-
 LOCAL_LDFLAGS := -llog
-
 include $(BUILD_SHARED_LIBRARY) 
