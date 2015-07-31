9f97363078202a2bcc77912f46448260cc35ac8b
Sasha <murzina93@gmail.com>
2015-07-31 14:11:29 +0600

    [*] Added JNI_OnLoad

--- jni/callbacks.c
+++ jni/callbacks.c
@@ -1,14 +1,12 @@
 #include "com_example_testandroidapp_MyDownloader.h"
 #include <android/log.h>
 
-JNIEXPORT void JNICALL Java_com_example_testandroidapp_MyDownloader_writeCallback
-	(JNIEnv *pEnv, jobject pThis, jint size){
+void writeCallback (JNIEnv *pEnv, jobject pThis, jint size){
 	__android_log_write(ANDROID_LOG_INFO, "Callbacks", "Downloaded");
 	return;
 }
 
-JNIEXPORT void JNICALL Java_com_example_testandroidapp_MyDownloader_progressCallback
-  (JNIEnv *pEnv, jobject pThis, jbyteArray byteArray, jint sizeTotal, jint sizeCurr){
+void progressCallback (JNIEnv *pEnv, jobject pThis, jbyteArray byteArray, jint sizeTotal, jint sizeCurr){
 	if (sizeTotal == 0){
 		//print only current size;
 		__android_log_write(ANDROID_LOG_INFO, "Callbacks", "Current size");
@@ -18,4 +16,24 @@ JNIEXPORT void JNICALL Java_com_example_testandroidapp_MyDownloader_progressCall
 		__android_log_write(ANDROID_LOG_INFO, "Callbacks", "Progress Callback");
 	}
 	return;
+}
+
+static JNINativeMethod methodTable[] = {
+	{"writeCallback", "(I)V", (void *)writeCallback},
+	{"progressCallback", "([BII)V", (void*)progressCallback}
+};
+
+jint JNI_OnLoad(JavaVM *vm, void *reserved){
+	JNIEnv* env;
+	if ((*vm)->GetEnv(vm, (void**)(&env), JNI_VERSION_1_6) != JNI_OK) {
+		return JNI_ERR;
+	}
+	jclass _class = (*env)->FindClass(env,"com/example/testandroidapp/MyDownloader");
+
+	if (_class){
+		(*env)->RegisterNatives(env, _class, methodTable, sizeof(methodTable) / sizeof(methodTable[0]) );
+	} else {
+		return JNI_ERR;
+	}
+	return JNI_VERSION_1_6;
 }
