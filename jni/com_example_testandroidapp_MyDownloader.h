d3a20830aaccbc7eadb3eca67d9700596079b18e
Sasha <murzina93@gmail.com>
2015-07-31 14:48:21 +0600

    [*] fixed MyDownloader.h

--- jni/com_example_testandroidapp_MyDownloader.h
+++ jni/com_example_testandroidapp_MyDownloader.h
@@ -14,7 +14,7 @@ extern "C" {
  * Method:    writeCallback
  * Signature: (I)V
  */
-JNIEXPORT void JNICALL Java_com_example_testandroidapp_MyDownloader_writeCallback
+JNIEXPORT static void JNICALL Java_com_example_testandroidapp_MyDownloader_writeCallback
   (JNIEnv *, jobject, jint);
 
 /*
@@ -22,7 +22,7 @@ JNIEXPORT void JNICALL Java_com_example_testandroidapp_MyDownloader_writeCallbac
  * Method:    progressCallback
  * Signature: ([BII)V
  */
-JNIEXPORT void JNICALL Java_com_example_testandroidapp_MyDownloader_progressCallback
+JNIEXPORT static void JNICALL Java_com_example_testandroidapp_MyDownloader_progressCallback
   (JNIEnv *, jobject, jbyteArray, jint, jint);
 
 #ifdef __cplusplus
