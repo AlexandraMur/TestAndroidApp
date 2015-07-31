ecef8b9540c261b058b79fabf2d533c438eef0ad
Sasha <murzina93@gmail.com>
2015-07-31 19:15:14 +0600

    [*] It works!

--- jni/test.c
+++ jni/test.c
@@ -2,22 +2,21 @@
 #include <pthread.h>
 #include <android/log.h>
 
-JavaVM *vm;
+static JavaVM *vm;
+static jclass myDownloader;
+static jmethodID downloadID;
 
 static void workFlow (){
 	JNIEnv *pEnv = NULL;
-
 	if ((*vm)->AttachCurrentThread(vm, &pEnv, NULL) != JNI_OK){
 		goto exit;
 	}
 
-	jclass myDownloader = (*pEnv)->FindClass(pEnv, "com/example/testandroidapp/MyDownloader");
 	if (!myDownloader){
 		__android_log_write(ANDROID_LOG_INFO, "test.c", "FAIL myDownloader");
 		goto exit;
 	}
 
-	jmethodID downloadID = (*pEnv)->GetStaticMethodID(pEnv, myDownloader, "download", "(Ljava/lang/String;)V");
 	if (!downloadID){
 		__android_log_write(ANDROID_LOG_INFO, "test.c", "FAIL downloadID");
 		goto exit;
@@ -47,6 +46,9 @@ jint JNI_OnLoad (JavaVM *vm_, void *reserved){
 	if ((*vm)->GetEnv(vm, (void**)(&env), JNI_VERSION_1_6) != JNI_OK) {
 		return JNI_ERR;
 	}
+	myDownloader = (*env)->FindClass(env, "com/example/testandroidapp/MyDownloader");
+	downloadID = (*env)->GetStaticMethodID(env, myDownloader, "download", "(Ljava/lang/String;)V");
+
 	jclass _class = (*env)->FindClass(env,"com/example/testandroidapp/MainActivity");
 
 	if (_class){
