49fe8848c1b695a75d25642f32ccdfc91e62b639
Sasha <murzina93@gmail.com>
2015-08-04 10:48:37 +0600

    [*] Fixed Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,22 +4,21 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS := -std=c99
 LOCAL_MODULE := downloader
 LOCAL_SRC_FILES := downloader/downloader.c
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -std=c99
 LOCAL_MODULE := cJSON
 LOCAL_SRC_FILES := parser/cJSON.c
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_CFLAGS += -std=c99
-LOCAL_SHARE_LIBRARIES := ljson
 LOCAL_MODULE := parser
 LOCAL_SRC_FILES := parser/parser.c
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
