d579d3a68e2f5c2b7e6e60945e428e002c76e51a
Sasha <murzina93@gmail.com>
2015-08-05 13:22:00 +0600

    [*] current state Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,28 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS := -std=c99
+LOCAL_CFLAGS := -std=gnu99
 LOCAL_MODULE := downloader
 LOCAL_SRC_FILES := downloader/downloader.c
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS := -std=c99
+LOCAL_CFLAGS := -std=gnu99
 LOCAL_MODULE := cJSON
 LOCAL_SRC_FILES := parser/cJSON.c
 include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS += -std=c99
+LOCAL_CFLAGS += -std=gnu99
 LOCAL_MODULE := parser
 LOCAL_SRC_FILES := parser/parser.c
+LOCAL_STATIC_LIBRARIES += cJSON
 include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS := -std=c99
+LOCAL_CFLAGS := -std=gnu99
 LOCAL_MODULE := callbacks
 LOCAL_SRC_FILES := callbacks.c
 LOCAL_LDFLAGS := -llog
@@ -30,8 +31,11 @@ include $(BUILD_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_CFLAGS := -std=c99
+LOCAL_CFLAGS := -std=gnu99
 LOCAL_MODULE := test
 LOCAL_SRC_FILES := test.c
+LOCAL_STATIC_LIBRARIES += parser
+LOCAL_STATIC_LIBRARIES += downloader
 LOCAL_LDFLAGS := -llog
 include $(BUILD_SHARED_LIBRARY) 
+  
