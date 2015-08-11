c56d7e6aace55ddec4498e124a244355fbc0c7ad
Mikhail Karev <Mikhail.Karev@elecard.ru>
2015-08-12 02:35:30 +0600

    [*] review/refactoring/bugfix

--- jni/test.c
+++ jni/test.c
@@ -6,48 +6,77 @@
 #include <downloader/downloader.h>
 #include <parser/parser.h>
 
-struct syncronize {
+#define LOG_TAG	"test"
+#define LOGI(fmt, ...) __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, "%s: " fmt, __func__, ## __VA_ARGS__)
+#define LOGW(fmt, ...) __android_log_print(ANDROID_LOG_WARN,  LOG_TAG, "%s: " fmt, __func__, ## __VA_ARGS__)
+#define LOGE(fmt, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "%s: " fmt, __func__, ## __VA_ARGS__)
+
+typedef struct
+{
 	int num;
 	pthread_mutex_t mutex;
 	pthread_cond_t cv;
 	int mutex_flag;
 	int cv_flag;
-};
+} Semaphore;
 
-static void my_complete (Downloader *d, void *args, int status, size_t number_files_in_stack){
-	__android_log_write(ANDROID_LOG_INFO, "test.c", "Downloaded");
-	return;
+static pthread_t g_thread;
+
+static void semaphore_wait(Semaphore *sync)
+{
+	pthread_mutex_lock(&sync->mutex);
+	while(sync->num){
+		pthread_cond_wait(&sync->cv, &sync->mutex);
+	}
+	pthread_mutex_unlock(&sync->mutex);
 }
 
-static void my_progress (Downloader *d, void *args, int64_t curr_size, int64_t total_size){
+static void semaphore_inc(Semaphore *sync)
+{
+	pthread_mutex_lock(&sync->mutex);
+	sync->num++;
+	pthread_mutex_unlock(&sync->mutex);
+}
+
+static void semaphore_dec(Semaphore *sync)
+{
+	pthread_mutex_lock(&sync->mutex);
+	sync->num--;
+	pthread_cond_signal(&sync->cv);
+	pthread_mutex_unlock(&sync->mutex);
+}
+
+static void my_complete (Downloader *d, void *args, int status, size_t number_files_in_stack)
+{
+	LOGI("Downloaded");
+	semaphore_dec((Semaphore*)args);
+}
+
+static void my_progress (Downloader *d, void *args, int64_t curr_size, int64_t total_size)
+{
 	if (total_size == 0){
 		//print only current size;
-		__android_log_write(ANDROID_LOG_INFO, "test.c", "Current size");
+		LOGI("Current size");
 	} else {
 		//print percents
 		int currPercent = (curr_size * 100) / total_size;
-		__android_log_write(ANDROID_LOG_INFO, "test.c", "Progress Callback");
+		LOGI("%d %%", currPercent);
 	}
 	return;
 }
 
-static void semaphore(struct syncronize *sync){
-	pthread_mutex_lock(&sync->mutex);
-		while(sync->num){
-			pthread_cond_wait(&sync->cv, &sync->mutex);
-		}
-		pthread_mutex_unlock(&sync->mutex);
-}
-
-static void workFlow (){
-	IDownloader_Cb my_callbacks;
-	my_callbacks.complete = &my_complete;
-	my_callbacks.progress = &my_progress;
+static void* workFlow (void *arg)
+{
+	IDownloader_Cb my_callbacks =
+	{
+		.complete = &my_complete,
+		.progress = &my_progress
+	};
 
 	Downloader *d = NULL;
 	Playlist *playlist = NULL;
 
-	struct syncronize sync = {0};
+	Semaphore sync = {0};
 
 	int mutex_error = pthread_mutex_init(&sync.mutex, NULL);
 	int cv_error = pthread_cond_init(&sync.cv, NULL);
@@ -72,63 +101,60 @@ static void workFlow (){
 	pthread_mutex_unlock(&sync.mutex);
 
 	int res = downloader_add(d, url, name);
-	if (res){
+	if (res) {
 		goto exit;
 	}
-
-	semaphore(&sync);
+	semaphore_wait(&sync);
+	LOGI("Playlist downloaded\n");
 
 	playlist = playlist_create();
-	if (!playlist){
+	if (!playlist) {
 		goto exit;
 	}
 
 	playlist_parse(playlist, name);
-	for(int i = 0; i < playlist->items_count; i++){
-		pthread_mutex_lock(&sync.mutex);
-		sync.num++;
-		pthread_mutex_unlock(&sync.mutex);
+	for (int i = 0; i < playlist->items_count; i++) {
+		semaphore_inc(&sync);
 		downloader_add(d, playlist->items[i].uri, playlist->items[i].name);
 	}
-	semaphore(&sync);
+	semaphore_wait(&sync);
 
 exit:
 	playlist_destroy(playlist);
 	downloader_destroy(d);
 
-	if (sync.mutex_flag){
+	if (sync.mutex_flag) {
 		pthread_mutex_destroy(&sync.mutex);
 	}
-	if (sync.cv_flag){
+	if (sync.cv_flag) {
 		pthread_cond_destroy(&sync.cv);
 	}
 	printf("finished\n");
+	return NULL;
 }
 
-static void nativeTest (){
-	pthread_t thread;
-	pthread_create(&thread, NULL, (void*)workFlow, NULL);
+static void nativeTest ()
+{
+	pthread_create(&g_thread, NULL, (void*)workFlow, NULL);
 }
 
-static JNINativeMethod methodTable[] = {
+static JNINativeMethod methodTable[] =
+{
 	{"nativeTest", "()V", (void *)nativeTest}
 };
 
-jint JNI_OnLoad (JavaVM *vm, void *reserved){
-	if (!vm){
+jint JNI_OnLoad (JavaVM *vm, void *reserved)
+{
+	if (!vm) {
 		return JNI_ERR;
 	}
-
-	int downl_onload = downloader_OnLoad(vm);
-	if (downl_onload != JNI_VERSION_1_6){
+	if (downloader_OnLoad(vm) != JNI_OK) {
 		return JNI_ERR;
 	}
-
 	JNIEnv* env;
 	if ((*vm)->GetEnv(vm, (void**)(&env), JNI_VERSION_1_6) != JNI_OK) {
 		return JNI_ERR;
 	}
-
 	jclass class = (*env)->FindClass(env,"com/example/testandroidapp/MainActivity");
 	if (!class){
 		return JNI_ERR;
